4c22e45adc0700528ec7f5b83e2e8af959f51648
Min <joyx666@gmail.com>
2018-04-01 01:32:25 -0400

    initial version

---
+++ src/main/java/guru/springframework/spring5Webapp/Spring5WebappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5Webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebappApplication.class, args);
+	}
+}
